71b6c9dad26b2a7f86a67f1656cdf72d48e46289
ViniciusAlvesGitHub <132413949+ViniciusAlvesGitHub@users.noreply.github.com>
2023-05-12 15:49:24 -0300

    primeiro commit

---
+++ Ex2/src/principal/Program.java
@@ -0,0 +1,49 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+		/*exercicio extra 1
+		int anoNascimento;
+		int anoAtual;
+		
+		System.out.print("Qual o ano atual? ");
+		anoAtual = leia.nextInt();
+		
+		System.out.print("Qual o ano do seu nascimento? ");
+		anoNascimento = leia.nextInt();
+		
+		System.out.print("Sua idade é: " + (anoAtual-anoNascimento));
+		*/
+		
+		
+		
+		
+		/*exercicio extra 2 
+		System.out.print("Qual o seu salário bruto? ");
+        double salarioBase = leia.nextDouble();
+
+        double inss = salarioBase * 0.05;
+
+        double ir = salarioBase * 0.11;
+        
+        double salarioLiquido = salarioBase - inss - ir;
+
+        System.out.println("Desconto de INSS (5%): R$ " + inss);
+        
+        System.out.println("Desconto de IR (11%): R$ " + ir);
+        
+        System.out.println("Salário líquido: R$ " + salarioLiquido);
+		*/
+		
+		
+	}
+
+}
